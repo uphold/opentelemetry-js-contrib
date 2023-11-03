f0fb216ed2fe053beeea610cdce2cbac33d8f2ce
Diogo Torres <diogo.torres@uphold.com>
2023-11-03 17:47:22 +0000

    Add @uphold/github-changelog-generator

--- .release-it.js
+++ .release-it.js
@@ -1,6 +1,7 @@
 module.exports = {
   git: {
-    changelog: 'git log --pretty=format:"* %s (%h)" ${from}...${to} .',
+    changelog:
+      'echo "## Changelog\\n\\n$(npx @uphold/github-changelog-generator -rtp ${npm.name} -f unreleased | tail -n +4 -f)"',
     commitMessage: 'Release ${npm.name}@${version}',
     requireBranch: 'master',
     requireCommits: true,
@@ -12,6 +13,7 @@ module.exports = {
   },
   hooks: {
     'after:bump': `
+      echo "$(npx @uphold/github-changelog-generator -rtp \${npm.name} -f \${version} -t v\${npm.name}@v\${version})\n$(tail -n +2 CHANGELOG.md)" > CHANGELOG.md &&
       npm run build
     `
   }
