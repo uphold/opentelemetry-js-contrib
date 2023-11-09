ef5487c25a86af10d241aca27862104eb23c4365
André Cruz <andremiguelcruz@msn.com>
2023-11-09 17:13:45 +0000

    Fix Span type in `onStart`

--- packages/opentelemetry-baggage-span-processor/src/baggage-span-processor.ts
+++ packages/opentelemetry-baggage-span-processor/src/baggage-span-processor.ts
@@ -1,5 +1,5 @@
-import { Context, propagation } from '@opentelemetry/api';
-import { NoopSpanProcessor, Span } from '@opentelemetry/sdk-trace-base';
+import { Context, Span, propagation } from '@opentelemetry/api';
+import { NoopSpanProcessor } from '@opentelemetry/sdk-trace-base';
 
 export class BaggageSpanProcessor extends NoopSpanProcessor {
   override onStart(span: Span, parentContext: Context): void {
