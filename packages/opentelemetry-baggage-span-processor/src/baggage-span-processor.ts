9f198d111cd0834853b8e6590aa387059f0f6984
André Cruz <andremiguelcruz@msn.com>
2024-01-24 19:28:44 +0000

    Do not depend on `@opentelemetry/sdk-trace-base`

--- packages/opentelemetry-baggage-span-processor/src/baggage-span-processor.ts
+++ packages/opentelemetry-baggage-span-processor/src/baggage-span-processor.ts
@@ -1,13 +1,22 @@
 import { Context, Span, propagation } from '@opentelemetry/api';
-import { NoopSpanProcessor } from '@opentelemetry/sdk-trace-base';
 
-export class BaggageSpanProcessor extends NoopSpanProcessor {
-  override onStart(span: Span, parentContext: Context): void {
+export class BaggageSpanProcessor {
+  onStart(span: Span, parentContext: Context): void {
     // Set all baggage entries as span attributes.
     const baggageEntries = propagation.getBaggage(parentContext)?.getAllEntries();
 
     baggageEntries?.forEach(([key, baggageEntry]) => {
       span.setAttribute(key, baggageEntry.value);
     });
+  }
+
+  onEnd(): void {}
+
+  shutdown(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  forceFlush(): Promise<void> {
+    return Promise.resolve();
   }
 }
